0bb1612e5cf67d113e10abc4364bc8ad0ffef779
vaniakazeko <kazeko.ivan.00@gmail.com>
2023-10-20 11:23:09 +0300

    server configuration

--- server/elerning/elerning/urls.py
+++ server/elerning/elerning/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,6 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/', include('user.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += doc_url
