5fcb0a88cb6cbade043f4c1d534fe43ef98413b1
vladoooos <vladverenikproga@gmail.com>
2023-10-21 10:52:21 +0300

    Add api register
    Добавил апи для регистрации

---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+import { ENDPOINTS } from 'src/app/core/endpoints';
+
+export const environment = {
+  production: true,
+  apiUrl: 'http://91.210.170.48/api/v1',
+  admin: 'http://91.210.170.48/',
+  api: ENDPOINTS,
+};
