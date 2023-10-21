b2b65b04bf02abe7e8c4f1e7ad5891604665587e
vaniakazeko <kazeko.ivan.00@gmail.com>
2023-10-21 20:34:31 +0300

    error text changes with the same email

--- server/elerning/user/serializers.py
+++ server/elerning/user/serializers.py
@@ -1,12 +1,16 @@
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserCreateSerializer as DjoserUserCreateSerializer
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
 
 class CustomUserCreateSerializer(DjoserUserCreateSerializer):
     repeat_password = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all(), message='Пользователь с таким email уже существует.')]
+    )
 
     class Meta(DjoserUserCreateSerializer.Meta):
         fields = DjoserUserCreateSerializer.Meta.fields + ('repeat_password',)
